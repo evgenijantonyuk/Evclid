c8c7e1bfe4586e427b617b217f4284f8e8bce323
evgenij antonyuk <evgenijantonyuk@gmail.com>
2022-05-17 04:15:38 +0700

    errors

--- index.js
+++ index.js
@@ -38,14 +38,11 @@ document.addEventListener('DOMContentLoaded', (e) => {
 })
 
 
-/*
-   ТАБЫ
-    */
 
 let all_tabs = document.querySelectorAll('.main__tabs_link'); //получаем все табы
 let tab_header = document.querySelector('.main__tab_title') //заголовок таба
 let tab_text = document.querySelector('.main__tab_text') //текст таба
-let tab_img = document.querySelector('.main__how-we-work.img img') //картинка таба
+let tab_img = document.querySelector('.img__adaptive img') //картинка таба
 
 
 
@@ -54,22 +51,22 @@ let tabs_db = {
     "1 шаг": {
         "header": "Проводим консультацию",
         "text": "Влечет за собой процесс внедрения и модернизации приоритизации разума над эмоциями. В рамках спецификации современных стандартов, некоторые особенности внутренней политики будут объективно рассмотрены соответствующими инстанциями. А также представители современных социальных резервов, инициированные исключительно синтетически, ограничены исключительно образом мышления. Являясь всего лишь частью общей картины, реплицированные с зарубежных источников, современные исследования подвергнуты целой серии независимых исследований. Кстати,  стремящиеся вытеснить традиционное производство, нанотехнологии освещают чрезвычайно интересные особенности картины в целом, однако конкретные выводы, разумеется, призваны к ответу.",
-        "img_url": "how-we-work(x1)_.jpg",
+        "img_url": "tab1.jpg",
     },
     "2 шаг": {
         "header": "Составляем смету",
         "text": "Внедрения и модернизации приоритизации разума над эмоциями. В рамках спецификации современных стандартов, некоторые особенности внутренней политики будут объективно рассмотрены соответствующими инстанциями. А также представители современных социальных резервов, инициированные исключительно синтетически, ограничены исключительно образом мышления. Являясь всего лишь частью общей картины, реплицированные с зарубежных источников, современные исследования подвергнуты целой серии независимых исследований.",
-        "img_url": "img/смета.jpg",
+        "img_url": "tab2.jpg",
     },
     "3 шаг": {
         "header": "Привлекаем подрядчиков",
         "text": "Идейные соображения высшего порядка, а также новая модель организационной деятельности требует анализа прогресса профессионального сообщества. Высокий уровень вовлечения представителей целевой аудитории является четким доказательством простого факта: высококачественный прототип будущего проекта напрямую зависит от дальнейших направлений развития. Разнообразный и богатый опыт говорит нам, что новая модель организационной деятельности говорит о возможностях системы массового участия. Принимая во внимание показатели успешности, постоянное информационно-пропагандистское обеспечение нашей деятельности позволяет выполнить важные задания по разработке прогресса профессионального сообщества.",
-        "img_url": "img/подряд.jpg",
+        "img_url": "tab3.jpg",
     },
     "4 шаг": {
         "header": "Инспектируем все этапы работ",
         "text": "Высокий уровень вовлечения представителей целевой аудитории является четким доказательством простого факта: высококачественный прототип будущего проекта напрямую зависит от дальнейших направлений развития. Разнообразный и богатый опыт говорит нам, что новая модель организационной деятельности говорит о возможностях системы массового участия. Принимая во внимание показатели успешности, постоянное информационно-пропагандистское обеспечение нашей деятельности позволяет.",
-        "img_url": "img/инсп-этапы-работ.jpg",
+        "img_url": "tab4.jpg",
     }
 }
 
@@ -87,25 +84,23 @@ all_tabs.forEach(function (el) {
 
         tab_header.innerText = tabs_db[el.innerText]['header'];
         tab_text.innerText = tabs_db[el.innerText]['text'];
-        tab_img.src = `url(../img/how-we-work(x1).jpg${tabs_db[el.innerText]['img_url']}) center center no-repeat`;
-        // tab_img.src = `url(../img/how-we-work(x1).jpg${tabs_db[el.innerText]['img_url']}) center center no-repeat`;
-        tab_img.style.backgroundSize = 'cover';
-
-        tab_header.innerText = tabs_db[el.innerText]['header'];
-        tab_text.innerText = tabs_db[el.innerText]['text'];
-        tab_img.src = `url(../img/counts.jpg${tabs_db[el.innerText]['img_url']}) center center no-repeat`;
-        //tab_img.src = 'url(${tabs_db[el.innerText][' + img_url + ']})';
-        tab_img.src.backgroundSize = 'cover';
-
-        tab_header.innerText = tabs_db[el.innerText]['header'];
-        tab_text.innerText = tabs_db[el.innerText]['text'];
-        tab_img.style.background = `url(../img/подряд.jpg${tabs_db[el.innerText]['img_url']}) center center no-repeat`;
-        tab_img.style.backgroundSize = 'cover';
-
-        tab_header.innerText = tabs_db[el.innerText]['header'];
-        tab_text.innerText = tabs_db[el.innerText]['text'];
-        tab_img.style.background = `url(../img/инсп-этапы-работ.jpg${tabs_db[el.innerText]['img_url']}) center center no-repeat`;
-        tab_img.style.backgroundSize = 'cover';
+        tab_img.innerText = tabs_db[el.innerText]['img_url']
+        // tab_img.style.backgroundSize = 'cover';
     });
+});
+
+let swiper = new Swiper('.swiper', {
+    slidesPerView: 1,
+    slidesPerGroupe: 1,
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true,
+    },
 });
 
