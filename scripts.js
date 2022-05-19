d2e26c573b507677f85f5c004a909c51f16caea3
evgenij antonyuk <evgenijantonyuk@gmail.com>
2022-05-20 04:48:58 +0700

    for checking

--- scripts.js
+++ scripts.js
@@ -133,10 +133,31 @@ let swiper = new Swiper('.swiper', {
     },
 });
 
+// Accordion
 
-document.querySelectorAll('.accordion-item__trigger').forEach((item) =>
-    item.addEventListener('click', () => {
-        const parent = item.parentNode;
-        
-        parent.classList.toggle('accordion-item__content:active');
-}))
+let accordion = document.getElementById('accordion');
+accordion.addEventListener('click', change);
+function change(event) {
+    let targ = event.target;
+    if (targ.tagName !== 'H3') return;
+    if (targ.classList.contains('select')) {
+        hideAll();
+    } else {
+        hideAll();
+        targ.classList.add('select');
+        showText(targ.nextElementSibling);
+    }
+}
+function hideAll() {
+    let h3El = accordion.querySelectorAll('h3');
+    let divEl = accordion.querySelectorAll('div');
+    for (let i = 0; i < h3El.length; i++) {
+        h3El[i].classList.remove('select');
+    }
+    for (let i = 0; i < divEl.length; i++) {
+        divEl[i].style.height = '0';
+    }
+}
+function showText(textEl) {
+    textEl.style.height = textEl.scrollHeight + 'px';
+}
